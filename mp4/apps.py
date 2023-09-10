99fb565e4f0c426220c914bb71cc79f7910d813b
RueiYuYang <rueiruei9985@gmail.com>
2023-09-10 15:23:25 +0800

    ver1.00.00

---
+++ mp4/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class Mp4Config(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "mp4"
---
+++ mp4/views.py
@@ -0,0 +1,61 @@
+from django.shortcuts import render
+import cv2
+import logging as log
+import datetime as dt
+# from django.http import HttpResponse
+from django.http import StreamingHttpResponse
+import numpy as np
+import time  # Import the time module
+
+cascPath = "C:/Users/User/Downloads/projectname-20230910T064614Z-001/projectname/haarcascade_frontalface_default.xml"
+faceCascade = cv2.CascadeClassifier(cascPath)
+log.basicConfig(filename='webcam.log', level=log.INFO)
+
+def face_detection(request):
+    # video_capture = cv2.VideoCapture("C:/Users/user/Desktop/new/projectname/30 Second.mp4")
+    video_capture = cv2.VideoCapture("C:/Users/User/Downloads/projectname-20230910T064614Z-001/projectname/turning.mp4")
+    anterior = 0
+
+    frame_delay = 0.075  # Set the delay between frames (in seconds)
+
+    while True:
+        # Capture frame-by-frame
+        ret, frame = video_capture.read()
+
+        if not ret:
+            break
+
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        faces = faceCascade.detectMultiScale(
+            gray,
+            scaleFactor=1.1,
+            minNeighbors=5,
+            minSize=(30, 30)
+        )
+
+        # Draw a rectangle around the faces
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+        if anterior != len(faces):
+            anterior = len(faces)
+            log.info("faces: "+str(len(faces))+" at "+str(dt.datetime.now()))
+
+        # Convert the frame to JPEG format
+        _, buffer = cv2.imencode('.jpg', frame)
+        frame = buffer.tobytes()
+
+        yield (b'--frame\r\n'
+                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+        # Introduce a delay between frames
+        time.sleep(frame_delay)
+
+def video_feed(request):
+    return  StreamingHttpResponse(face_detection(request), content_type='multipart/x-mixed-replace; boundary=frame')
+
+
+
+def video_stream(request):
+    return render(request, 'video_stream.html', {'video_stream_url': '/video_feed/'})
