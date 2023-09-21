3014e27fe3cfddd51b8ad73aee77728d8a66aa33
Brian Liu <billy850424@gmail.com>
2023-09-21 13:13:40 +0800

    feat: line notify, parallel execute

--- mylib/line.py
+++ mylib/line.py
@@ -1,73 +1,117 @@
 import requests
 import datetime
 from time import time
+import threading
+import queue
 
+class Notify:
+    def __init__(self):
+        """
+        Line Notify\n
+        detections (list): 物件偵測回傳的list\n
+        kt (float): 姿勢維持多久觸發警報\n
+        imageFile (dict): 危險姿勢截圖\n
+        si (float): 警報間隔
+        token (str): line通知令牌  
+        """
+        self.q = queue.Queue()
+        self.q1 = queue.Queue()
+        self.send = 'No'
+        self.old_result = None
+        self.first_alarm = None
+        self.data = None
+    # 發通知
+    def send_work(self, data, imageFile):
+        while True:
+            task = self.q.get()
 
-def line_notify(tn,detections,kt,imageFile,si):
-    """
-    Line Notify\n
-    tn (str): 是否開啟通知\n
-    detections (list): 物件偵測回傳的list\n
-    kt (float): 姿勢維持多久觸發警報\n
-    imageFile (dict): 危險姿勢截圖\n
-    si (float): 警報間隔
-    token (str): line通知令牌  
-    """
-    url = "https://notify-api.line.me/api/notify"
-    token = "ubJnBPx5JLWX5FqXLmfIxmUUsxfFcCE402PlICSJeNi"
-    headers = {'Authorization': 'Bearer ' + token}
-    global old_result
-    global first_alarm
-    global send_time
-    if tn == "true":
+            url = "https://notify-api.line.me/api/notify"
+            token = "DiLXmcVgZzBv4lEg7jYRCDxksSK4JPl6EZbNweXFrqi"
+            headers = {'Authorization': 'Bearer ' + token}
+
+            try:
+                if task:
+                    respon = requests.post(url, headers=headers, data=task[0], files=task[1])
+                    print(f'Working')
+                    # print(f'響應內容：{respon.text}')
+                    # print(f'{respon} is finished')
+                    send_time = time()
+                    self.q1.put(send_time)
+            except Exception as e:
+                print("An error occurred:", str(e))
+            finally:
+                print('Send_Done')
+                self.q.task_done()
+                
+    def get_send_time(self):
         try:
-            print('0')
-            print(detections)
-            test_result = detections[0][0]
-            print(test_result)
-            now_time = time()
-            if test_result == "Danger":
-                now_alarm = time()
-                print(test_result)
-                if test_result == old_result:
-                    print('1')
-                    if now_alarm - first_alarm > kt:
-                        print('2')
-                        if send_time is None:
-                            print('3')
-                            now = datetime.datetime.now()
-                            formatted_time = now.strftime("%Y-%m-%d %H:%M:%S")
-                            data = {
-                                "message": f'Warning: Your baby is at risk!\n'
-                                           f'姿勢維持時間:{now_alarm - first_alarm}\n'
-                                           f'現在時間:{formatted_time}'                                    
-                            }
-                            respon = requests.post(url, headers=headers, data=data, files=imageFile)
-                            #print(respon.status_code)    
-                            send_time = time() 
-                        elif now_alarm - send_time > si:
-                            print('4')
-                            now = datetime.datetime.now()
-                            formatted_time = now.strftime("%Y-%m-%d %H:%M:%S")                           
-                            data = {
-                                "message": f'Warning: Your baby is at risk!\n'
-                                           f'姿勢維持時間:{now_alarm - first_alarm}\n'
-                                           f'現在時間:{formatted_time}'                                    
-                            }
-                            respon = requests.post(url, headers=headers, data=data, files=imageFile)                        
-                            send_time = time()
-                else:
-                    old_result = test_result
-                    first_alarm = now_time                           
-                    send_time = None         
+            if self.send == 'Yes':
+                return self.q1.get()
             else:
-                old_result = test_result
-                send_time = None
-                print("Your baby is fine")                
-        except:
-            print("Baby probably not here")
-    else:            
-        old_result = None
-        first_alarm = None
-        send_time = None
-    return old_result, first_alarm, send_time
+                return None
+        except Exception as e:
+                print("An error occurred:", str(e))
+        return None
+        
+    def line_notify(self, detections, kt, si, tn, send_time):
+        if tn == "true":
+            try:
+                test_result = detections[0][0]
+                now_time = time()
+                if test_result == "Danger":
+                    now_alarm = time()
+                    if test_result == self.old_result:
+                        if now_alarm - self.first_alarm > kt:
+                            # print('超過持續時間了')
+                            # print(send_time)
+                            # print(si)
+                            # if send_time is not None:
+                            #     print(now_alarm - send_time)
+                            if send_time is None:
+                                # print('發第一次通知')
+                                now = datetime.datetime.now()
+                                formatted_time = now.strftime("%Y-%m-%d %H:%M:%S")
+                                self.data = {
+                                    "message": f'Warning: Your baby is at risk!\n'
+                                            f'姿勢維持時間:{round(now_alarm - self.first_alarm)}秒鐘\n'
+                                            f'現在時間:{formatted_time}'                                    
+                                }
+                                self.send = 'Yes'
+                            elif now_alarm - send_time > si:
+                                # print('第n次通知')
+                                now = datetime.datetime.now()
+                                formatted_time = now.strftime("%Y-%m-%d %H:%M:%S")                           
+                                self.data = {
+                                    "message": f'Warning: Your baby is at risk!\n'
+                                            f'姿勢維持時間:{round(now_alarm - self.first_alarm)}秒鐘\n'
+                                            f'現在時間:{formatted_time}'                                    
+                                }
+                                self.send = 'Yes'
+                        else:
+                            print('翻過去了，持續時間還沒到')
+                            self.old_result = test_result
+                            self.send = 'No'
+                            self.data = None
+                    else:
+                        print('剛翻過去，還不用通知')
+                        self.old_result = test_result
+                        self.first_alarm = now_time
+                        self.send = 'No'
+                        self.data = None
+                else:
+                    self.old_result = test_result
+                    self.send = 'No'
+                    self.data = None
+                    print("Your baby is fine")
+            except:
+                self.old_result = None
+                self.first_alarm = None
+                self.send = 'No'
+                self.data = None
+                print("Baby probably not here")
+        else:
+            print("Notifications are not turned on")
+            self.old_result = None
+            self.first_alarm = None
+            self.send = 'No'
+            self.data = None
--- webcam/views.py
+++ webcam/views.py
@@ -9,8 +9,8 @@ from django.http import StreamingHttpResponse
 from mylib import computer_vision as cv
 from mylib.deploy_model import Yolo
 from api.views import *
-from mylib.line import line_notify
-from mylib.Mediapipe import MediapipeDetector # 導入 Mediapipe Detector
+from mylib.line import Notify
+#from mylib.Mediapipe import MediapipeDetector # 導入 Mediapipe Detector
 
 # logger
 log.basicConfig(filename="webcam.log", level=log.INFO)
@@ -19,8 +19,7 @@ def obj_detection_webcam(request):
     # Vars
     yolo = Yolo()
     # Mediapipe 偵測器載入
-    MediapipeDetector_working = MediapipeDetector()
-
+    #MediapipeDetector_working = MediapipeDetector()
     # Parameters
     cfg_file = "cfg/yolov4-cfg-train_ori.cfg"  # 模型配置
     data_file = "data/pose_ori.data"  # 資料集路徑
@@ -34,16 +33,23 @@ def obj_detection_webcam(request):
     yolo_loadNet_thread.setDaemon(True)
     # 執行緒開始運行
     yolo_loadNet_thread.start()
-
-    # Open a connection to the default webcam (usually index 0)
-    # video_capture = cv2.VideoCapture(0)
-    video_capture, video_writer, video_height, video_width = cv.cam_init(0)
-    anterior = 0
-
+    
+    # 建構Line notify副執行緒
+    task = Notify()
     # global param for line notify
-    old_result = None
-    first_alarm = None
+    task.old_result = None
+    task.first_alarm = None
+    task.data = None
     send_time = None
+    imageFile = None
+    old_data = None
+    send_thread = threading.Thread(target = task.send_work, args=(task.data, imageFile), daemon=True)
+    send_thread.start()
+        
+    # Open a connection to the default webcam (usually index 0)
+    # video_capture = cv2.VideoCapture(0) 
+    video_capture, video_writer, video_height, video_width = cv.cam_init("2023-09-20.mp4")
+    anterior = 0
     
     # frame_delay = 0.075  # Set the delay between frames (in seconds)
     
@@ -57,7 +63,7 @@ def obj_detection_webcam(request):
         start_time = time()
         # TODO: 從網頁端抓取使用者變數
         global_param = get_param()
-        print(global_param)
+        #print(global_param)
         user_thresh = float(global_param["acc"])
         kt = float(global_param["dangertime"])
         si = float(global_param["warningtime"])
@@ -74,12 +80,12 @@ def obj_detection_webcam(request):
         # frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
         # TODO: BradyFan 需確認要 Yolo 最終是用原圖疊加骨架或者空白背景骨架
-        detection_face, detection_pose = MediapipeDetector_working.run_all(frame) # 透過模型偵測 frame_rgb 的臉跟骨架
+        #detection_face, detection_pose = MediapipeDetector_working.run_all(frame) # 透過模型偵測 frame_rgb 的臉跟骨架
         # blank_frame = MediapipeDetector_working.draw_blank(detection_face, detection_pose) # 繪製空白背景骨架圖
-        overlay_frame = MediapipeDetector_working.draw_overlay(detection_face, detection_pose) # 原圖疊加骨架圖
+        #overlay_frame = MediapipeDetector_working.draw_overlay(detection_face, detection_pose) # 原圖疊加骨架圖
         
         # blank_frame_rgb = cv2.cvtColor(blank_frame, cv2.COLOR_BGR2RGB) # 轉換色彩空間 BGR -> RGB
-        overlay_frame_rgb = cv2.cvtColor(overlay_frame, cv2.COLOR_BGR2RGB) # 轉換色彩空間 BGR -> RGB
+        overlay_frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB) # 轉換色彩空間 BGR -> RGB
 
         # Yolo Object Detection
         image_detection, detections = yolo.Object_Detect(overlay_frame_rgb, user_thresh)
@@ -112,8 +118,13 @@ def obj_detection_webcam(request):
         imageFile = {'imageFile' : frame}   # 設定圖片資訊
 
         # TODO: Line Notify
-        old_result, first_alarm,  send_time = line_notify(tn,detections,kt,imageFile,si) 
-        print(old_result, first_alarm,  send_time)
+        task.line_notify(detections, kt, si, tn, send_time)
+        if task.data is not None:
+            if old_data is None or task.data != old_data:
+                old_data = task.data
+                task.q.put((task.data, imageFile), block=True)
+                send_time = task.get_send_time()
+        print(task.old_result, task.first_alarm,  send_time)
 
         yield (b"--frame\r\n" b"Content-Type: image/jpeg\r\n\r\n" + frame + b"\r\n")
 
